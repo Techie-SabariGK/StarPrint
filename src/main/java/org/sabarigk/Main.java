d309706e2f2761bbb2cf6124691310671756ccc9
G <919823@cognizant.com>
2024-08-23 14:55:05 +0530

    First-commit

---
+++ src/main/java/org/sabarigk/Main.java
@@ -0,0 +1,18 @@
+package org.sabarigk;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int n=4;
+        for(int i=1;i<=n;i++){
+            for(int j=i;j<n;j++){
+                System.out.print(" ");
+            }
+            for(int k=1;k<=(2*i-1);k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
